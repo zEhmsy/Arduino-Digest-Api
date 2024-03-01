b16d52764959b77fcc953f8c8a47118c3659df8e
Giuseppe Turturro <89219888+zEhmsy@users.noreply.github.com>
2024-03-01 12:05:54 +0100

    Create config.h

---
+++ apidigest/config.h
@@ -0,0 +1,9 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+const char *username = "<USERNAME>"; // Device username
+const char *password = "<PASSWORD>"; // Device password
+const char *server_pointer = "http://192.168.1.200"; // Device address
+//const char *uri = "/cgi-bin/example"; // Device api
+
+#endif
